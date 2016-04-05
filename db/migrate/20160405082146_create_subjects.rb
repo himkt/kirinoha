59c132fef248cbdab07517b71caddc72e6473be1
himkt <himkt@klis.tsukuba.ac.jp>
2016-04-05 17:31:25 +0900

    Create model Subject

---
+++ db/migrate/20160405082146_create_subjects.rb
@@ -0,0 +1,20 @@
+class CreateSubjects < ActiveRecord::Migration
+  def change
+    create_table :subjects do |t|
+      t.string :code
+      t.string :title
+      t.float :credits
+      t.string :grades
+      t.string :terms
+      t.string :daytimes
+      t.string :location
+      t.string :instructors
+      t.text :info
+      t.binary :ca
+      t.text :condition
+      t.string :alternative
+
+      t.timestamps null: false
+    end
+  end
+end
