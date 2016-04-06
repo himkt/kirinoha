67faa46989d772d4ce01c0f2c05c00c8110f7686
himkt <himkt@klis.tsukuba.ac.jp>
2016-04-07 01:14:00 +0900

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'course#index'
+
+  get 'course/search'
+
+  get 'course/detail/:id' => 'course#detail'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
